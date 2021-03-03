f328d3a574777ec669a22f8bae6975baa2e3aab1
nihei-terra116 <73629863+nihei-terra116@users.noreply.github.com>
2021-03-03 13:56:24 +0900

    ThreadBean.java(更新)
    更新しました。

--- ThreadBean.java
+++ ThreadBean.java
@@ -1,12 +1,13 @@
-package tera;
+package bean;
 
 import java.io.Serializable;
 
-public class ThreadBean implements Serializable {
+public class ThreadBean implements Serializable{
     private String title;
-    private String date;
     private String tag;
     private String comment;
+
+    public ThreadBean(){}
 
     public String getTitle(){
         return title;
@@ -14,31 +15,17 @@ public class ThreadBean implements Serializable {
     public void setTitle(String title) {
         this.title = title;
     }
-
-    public String getDate(){
-        return date;
-    }
-    public void setDate(){
-        this.date=date;
-    }
-
     public String getTag(){
         return tag;
     }
     public void setTag(String tag) {
         this.tag = tag;
     }
-
     public String getComment(){
-        return comment;
-    }
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
+		return comment;
+	}
+	public void setComment(String Comment){
+		this.comment = comment;
+	}
 
-    public void setAll(String title,String date,String comment){
-        this.title=title;
-        this.date=date;
-        this.comment=comment;
-    }
 }
